16bbb66c84c6513c9eea3ded56c551b0f9f6eed5
ChrisJVoss <chrisvoss92@gmail.com>
2017-10-18 16:47:10 -0700

    Square now takes props from Board.

--- public/main.js
+++ public/main.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import './styles.css'
 
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => this.props.onClick({value: 'X'})}>
+        {this.props.value}
       </button>
-    );
+    )
   }
 }
 
 class Board extends React.Component {
+  construcor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+  }
   renderSquare(i) {
-    return <Square />;
+    return <Square value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}/>
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: X'
 
     return (
       <div>
@@ -39,7 +45,7 @@ class Board extends React.Component {
           {this.renderSquare(8)}
         </div>
       </div>
-    );
+    )
   }
 }
 
@@ -55,7 +61,7 @@ class Game extends React.Component {
           <ol>{/* TODO */}</ol>
         </div>
       </div>
-    );
+    )
   }
 }
 
@@ -63,61 +69,5 @@ class Game extends React.Component {
 
 ReactDOM.render(
   <Game />,
-  document.getElementById('root')
-);
-
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-    };
-  }
-
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-
-  class Square extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: null,
-      };
-    }
-
-    render() {
-      return (
-        <button className="square" onClick={() => alert('click')}>
-          {this.props.value}
-        </button>
-      );
-    }
-  }
+  document.getElementById('react-component')
+)
