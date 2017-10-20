1561bc0beaab6b6d5b52a58b64578e2c1f211d45
ChrisJVoss <chrisvoss92@gmail.com>
2017-10-19 18:30:31 -0700

    Fixed issue with player's turn not updating.

--- public/main.js
+++ public/main.js
@@ -6,40 +6,48 @@ function Square(props) {
     <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
-  )
+  );
 }
 
 class Board extends React.Component {
-  construcor(props) {
-    super(props)
+  constructor() {
+    super();
     this.state = {
       squares: Array(9).fill(null),
-      xIsNext: true
-    }
+      xIsNext: true,
+    };
   }
+
   handleClick(i) {
-    const squares = this.state.squares.slice()
+    const squares = this.state.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
-      return
+      return;
     }
-    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
-    xIsNext: !this.state.xIsNext})
+      xIsNext: !this.state.xIsNext,
+    });
   }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}
-    onClick={() => this.handleClick(i)}/>
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares)
-    let status
+    const winner = calculateWinner(this.state.squares);
+    let status;
     if (winner) {
-      status = 'Winner: ' + winner
+      status = 'Winner: ' + winner;
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -59,7 +67,7 @@ class Board extends React.Component {
           {this.renderSquare(8)}
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -75,7 +83,7 @@ class Game extends React.Component {
           <ol>{/* TODO */}</ol>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -84,24 +92,24 @@ class Game extends React.Component {
 ReactDOM.render(
   <Game />,
   document.getElementById('react-component')
-)
+);
 
-function calculateWinner(squares){
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ]
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i]
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a]
-      }
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
-    return null
+  }
+  return null;
 }
